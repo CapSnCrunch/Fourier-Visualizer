c59c40f44d1ab6975609019c09f67afb05d9d655
Samuel Perales <samuelperales4@gmail.com>
2021-01-10 20:22:13 -0600

    Initial commit

---
+++ inverse_spectrum.py
@@ -0,0 +1,139 @@
+import cv2 # pip install opencv-python
+import numpy as np # pip install numpy
+import pygame # pip install pygame
+from pygame.locals import *
+import pygame.surfarray as surfarray
+
+'''
+    Draw on the pygame window and see the spectrum of the 512x512 image update in real time!
+    You can mess with the view_size parameter to zoom in and out on the spectrum.
+    Press r to refresh your drawing window.
+    Press numpad values to load preselected images.
+    Scrolling will shift the drawing color along a grey scale all the way up to white for erasing.
+'''
+
+fft2, ifft2 = np.fft.fft2, np.fft.ifft2
+fftshift, ifftshift = np.fft.fftshift, np.fft.ifftshift
+
+current_folder = 'Other/Photo Effects/Images/' # Change depending on your local setup
+images = ['bee.jpg', 'flowers.jpg', 'trees.jpg', 'forest.jpg', 'nebula.jpg', 'water.jpg', 'clouds.jpg', 'mountain.jpg', 'city.jpg', 'buildings.jpg']
+
+# Parameters
+width = 500
+height = 500
+view_size = 250
+save_directory = 'Other/Photo Effects/Spectra/'
+
+img = np.zeros((width, height))
+
+def main():
+    pygame.init()
+
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+
+    #WHITE = (0, 0, 0)
+    #BLACK = (255, 255, 255)
+
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('DRAW HERE')
+    screen.fill(WHITE)
+
+    color = BLACK
+
+    #pygame.draw.line(screen, BLACK, (0, 250), (500, 250), 1)
+    #pygame.draw.line(screen, BLACK, (250, 0), (250, 500), 1)
+
+    center = (width / 2, height / 2)
+
+    mouse_position = (0, 0)
+    drawing = False
+    last_pos = None
+    clock = pygame.time.Clock()
+
+    saved = False
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            elif event.type == MOUSEMOTION:
+                if drawing:
+                    mouse_position = pygame.mouse.get_pos()
+                    # if mouse_position[0] < 256 and mouse_position[1] < 256:
+                    if True:
+                        if last_pos is not None:
+                            pygame.draw.line(screen, color, last_pos, mouse_position, 1)
+                        last_pos = mouse_position
+
+            elif event.type == MOUSEBUTTONUP:
+                mouse_position = (0, 0)
+                last_pos = None
+                drawing = False
+
+            elif event.type == MOUSEBUTTONDOWN:
+                drawing = True
+                # Check for scrolling up or down
+                if event.button == 5: 
+                    color = tuple([min(x + 10, 255) for x in color])
+                elif event.button == 4: 
+                    color = tuple([max(x - 10, 0) for x in color])
+
+            elif event.type == KEYDOWN:
+                print(event.key)
+                # Refresh the screen
+                if event.key == 114:
+                    screen.fill(WHITE)
+                    saved = False
+                    #pygame.draw.line(screen, BLACK, (0, 250), (500, 250), 1)
+                    #pygame.draw.line(screen, BLACK, (250, 0), (250, 500), 1)
+                # Load image
+                elif 1073741913 <= event.key <= 1073741922:
+                    try:
+                        if event.key == 1073741922:
+                            img = pygame.image.load(current_folder + images[0])
+                        else:
+                            img = pygame.image.load(current_folder + images[event.key - 1073741912]) 
+                        screen.blit(img, (0, 0))
+                    except:
+                        screen.fill(WHITE)
+
+                elif event.key == 115 and not saved:
+                    cv2.imwrite(save_directory + 'spectra.jpg', img)
+                    saved = True
+
+        pygame.display.update()
+        clock.tick(30) # Limit the frame rate to 30 FPS.
+
+        # Grab current drawing
+        x = pygame.surfarray.array2d(screen)
+        # x = x[0:250, 0:250]
+
+        # Normalization
+        # x = x / np.max(x)
+
+        # Take spectrum of current drawing
+        x = x.transpose()
+        x = fftshift(ifft2(x))
+
+        # Grab center view window from current spectrum
+        img = np.array(x, dtype = np.uint8)
+        img = img[int(center[0] - view_size / 2):int(center[0] + view_size / 2), int(center[1] - view_size / 2):int(center[1] + view_size/2)]
+
+        # Record max for rescaling later
+        img_max = np.max(img)
+
+        # Normalization
+        img = img / img_max
+
+        print(np.min(img), np.max(img))
+
+        # Scale view window back up to width x height
+        img = np.kron(img, np.ones((int(width / view_size), int(height / view_size))))
+
+        cv2.imshow('Image', img)
+        cv2.waitKey(30) # Gives time to handle user input
+
+main()
---
+++ weave.py
@@ -0,0 +1,31 @@
+import random
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+current_folder = 'Other/Photo Effects/Images/' # Change depending on your local setup
+photos = ['bee.jpg', 'flowers.jpg', 'trees.jpg', 'forest.jpg', 'nebula.jpg', 'water.jpg', 'clouds.jpg', 'mountain.jpg', 'city.jpg', 'buildings.jpg']
+
+A, B = (random.randint(0, len(photos)-1), random.randint(0, len(photos)-1))
+
+# Good ones: (1, 4), (2, 6), (3, 5), (4, 5), (4, 6)
+# A, B = (0, 1)
+
+block_size = 1
+
+img = Image.open(current_folder + photos[A])
+img2 = Image.open(current_folder + photos[B])
+img_array = np.array(img)
+img2_array = np.array(img2)
+
+n, m, channels = img_array.shape
+
+for i in range(int(n/block_size)):
+    for j in range(int(m/block_size)):
+        if (i + j) % 2 == 0:
+            img_array[i*block_size:(i+1)*block_size, j*block_size:(j+1)*block_size, :] = np.zeros((block_size, block_size, 3)) #[np.zeros(block_size), np.zeros(block_size), [0, 0, 0]]
+        else:
+            img2_array[i*block_size:(i+1)*block_size, j*block_size:(j+1)*block_size, :] = np.zeros((block_size, block_size, 3)) #[np.zeros(block_size), np.zeros(block_size), [0, 0, 0]]
+
+plt.imshow(img_array + img2_array)
+plt.show()
